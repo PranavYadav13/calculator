37e802cea2e9c6235af29ebe9ab3c8d49b1ac1b9
PranavYadav13 <pranavyadav292@gmail.com>
2026-01-30 11:12:47 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,10 @@
+class Calculator{
+  public statuc void main(String args[])
+  {
+    int n1=Integer.parseInt(args[0]);
+    int n2=Integer.parseInt(args[1]);
+    int r1=n1+n2;
+    int r2=n1-n2;
+    int r3=n1*n2;
+    int r4=n1/n2;
+  }
